0222b37cb453a67fad6d33468b644a3f1cdace62
Leo Ramos Andrade <leo.ramos182@hotmail.com>
2022-08-29 18:12:59 -0300

    Criado entity base

---
+++ Domain/Entities/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public abstract class EntityBase : IEquatable<EntityBase>
+    {
+        public EntityBase()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+
+        public bool Equals(EntityBase? other)
+        {
+            return Id == other?.Id;
+        }
+    }
+}
---
+++ Domain/Entities/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    internal class Todo : EntityBase
+    {
+        public string? Title { get; private set; }
+        public string? User { get; private set; }
+        public bool Done { get; private set; }
+        public DateTime Date { get; private set; }
+    }
+}
